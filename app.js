44cd4aba015d0874f8d10d9431f1dfb0b3bb2727
Aniket Kumar <19206@iiitu.ac.in>
2021-04-06 03:47:44 +0530

    Redoing

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ expressSanitizer = require("express-sanitizer"),
             User = require("./models/user"),
       bodyParser = require("body-parser");
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-
-
 // var session = require('express-session');
 // var FileStore = require('session-file-store')(session);
 
@@ -41,26 +37,11 @@ var blogSchema = new mongoose.Schema({
 });
 var Blog = mongoose.model("Blog", blogSchema);
 
-//-momery unleaked---------
-app.set('trust proxy', 1);
-
-app.use(session({
-cookie:{
-    secure: true,
-    maxAge:60000
-       },
-store: new RedisStore(),
-secret: 'secret',
-saveUninitialized: true,
-resave: false
+app.use(require("express-session")({
+	secret : "New Delhi is the Capital of India",
+	resave : false,
+	saveUninitialized : false
 }));
-
-app.use(function(req,res,next){
-if(!req.session){
-    return next(new Error('Oh no')) //handle error
-}
-next() //otherwise continue
-});
 
 // app.use(session({
 //     store: new FileStore({}),
